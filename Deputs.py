98afa886ac20f07ca18e037b0277ac5760e11f0a
Clarissa Treptow <168579472+Claridade74@users.noreply.github.com>
2024-05-22 14:49:49 -0300

    Update Deputs.py

--- Deputs.py
+++ Deputs.py
@@ -1,3 +1,12 @@
 import streamlit as st
 
 st.title('Câmara dos Deputados')
+
+with st.sidebar:
+  st.header('Melhor site de dados da camara')
+  st.write('')
+  st.caption('Criado por Manuela, Maju e Clarissa')
+
+st.write('Nosso aplicativo tem como foco mostrar os dados da camara')
+
+#para usar abas, use tabs

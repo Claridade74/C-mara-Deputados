e8717f3a667d128d3fdfedbf9f8290ab6d940b6c
Mateus Pestana <matheus.pestana@iesp.uerj.br>
2024-06-09 19:56:42 -0300

    Update ecopolitica.py

--- ecopolitica.py
+++ ecopolitica.py
@@ -76,27 +76,36 @@ with col2:
     ).properties(height=360)    
     st.altair_chart(grafico_projetos, use_container_width=True)
 
+
+st.subheader('Últimos projetos')
+st.write('Abaixo, você encontra os últimos projetos iniciados que são relacionados ao meio ambiente.')
+st.dataframe(df_filtrado[['siglaTipo', 'numero', 'ano', 'ementa']].tail(10), use_container_width=True)
+
+
 st.subheader('Frentes relacionadas ao Meio Ambiente')
 st.write('Abaixo, você encontra as frentes parlamentares relacionadas ao meio ambiente, bem como seus coordenadores e contato.')
-url_base = 'https://dadosabertos.camara.leg.br/api/v2/frentes?idLegislatura=57'
-content = session.get(url_base).json()
-frentes = []
-for item in content['dados']:
-    frentes.append(item)
-while content['links'][1]['rel'] == 'next':
-    print(content['links'][1]['href'])
-    url = content['links'][1]['href']
-    content = session.get(url).json()
+with st.spinner('Baixando dados das frentes...'):
+    url_base = 'https://dadosabertos.camara.leg.br/api/v2/frentes?idLegislatura=57'
+    content = session.get(url_base).json()
+    frentes = []
     for item in content['dados']:
         frentes.append(item)
-df_frentes = pd.DataFrame(frentes)
-df_frentes = df_frentes[df_frentes['titulo'].str.contains('agro|ambiente|lixo|rural|amaz.nia|verde|ecolog|campo|nutricional|bambu', na=False, regex=True, case=False)]
-for i, row in df_frentes.iterrows():
-    frente_info = session.get(row['uri']).json()
-    coordenador = frente_info['dados']['coordenador']
-    with st.container(border=True):
-        subcol1, subcol2 = st.columns([1, 7])
-        subcol1.image(coordenador['urlFoto'], width=100)
-        subcol2.subheader(row['titulo'])
+    while content['links'][1]['rel'] == 'next':
+        print(content['links'][1]['href'])
+        url = content['links'][1]['href']
+        content = session.get(url).json()
+        for item in content['dados']:
+            frentes.append(item)
+    df_frentes = pd.DataFrame(frentes)
+    df_frentes = df_frentes[df_frentes['titulo'].str.contains('agro|ambiente|lixo|rural|amaz.nia|verde|ecolog|campo|nutricional|bambu', na=False, regex=True, case=False)]
+    for i, row in df_frentes.iterrows():
+        frente_info = session.get(row['uri']).json()
+        coordenador = frente_info['dados']['coordenador']
+        with st.container(border=True):
+            subcol1, subcol2 = st.columns([1, 7])
+            subcol1.image(coordenador['urlFoto'], width=100)
+            subcol2.subheader(row['titulo'])
+            subcol2.write(f'Coordenador: {coordenador["nome"]} - {coordenador["siglaPartido"]}-{coordenador["siglaUf"]}')
+            subcol2.caption(f'Contato: {coordenador["email"]}')
         subcol2.write(f'Coordenador: {coordenador["nome"]} - {coordenador["siglaPartido"]}-{coordenador["siglaUf"]}')
         subcol2.caption(f'Contato: {coordenador["email"]}')

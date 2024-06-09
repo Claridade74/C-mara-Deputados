1f8e767a75ce420f770576224611829f357f99b1
Clarissa Treptow <168579472+Claridade74@users.noreply.github.com>
2024-06-09 17:49:53 -0300

    Update ecopolitica.py

--- ecopolitica.py
+++ ecopolitica.py
@@ -27,45 +27,32 @@ with st.sidebar:
     st.caption('Projeto desenvolvido por Maria Julia de Oliveira, Manuela Muniz e Clarissa Treptow, sob supervisão do Prof. Matheus C. Pestana')
     st.caption('FGV ECMI')
 
-
 def baixaProposicoes(ano=2024):
     arquivo = requests.get(f'http://dadosabertos.camara.leg.br/arquivos/proposicoes/xlsx/proposicoes-{ano}.xlsx')
     with open(f'proposicoes.xlsx', 'wb') as f:
         f.write(arquivo.content)
     return pd.read_excel('proposicoes.xlsx')
   
+with st.spinner('Buscando base atualizada'):
+  df = baixaProposicoes(2024)
+  keywords_busca = r'poluição|sucat[ae]|reciclagem|dejeto|pesca|praia|pecu[áa]ri|\bgado|descarte|queimada|lixo|meio ambiente|funai|ind.gena|garimpo|\bminera'
+  df_filtrado = df[(df['codTipo'].isin([139, 291, 550, 554, 560, 561, 553, 552, 557, 632, 692, 693, 657, 658, 659, 660, 140, 390,141, 142, 136, 138])) &
+   ((df['ementa'].str.contains(keywords_busca, case=False, na=False, regex=True)) |
+   (df['keywords'].str.contains(keywords_busca, case=False, na=False, regex=True)))]
+  
 with open('modelo.pkl', 'rb') as arquivo:
   modelo = pickle.load(arquivo)
-
-with st.spinner('Buscando base atualizada'):
-   
-df_all = pd.read_parquet('df_clarissa_classificado.parquet', index=False)
-df_all = df_all[['ano', 'ementa', 'keywords']]
-df_all.dropna(subset=['ementa'], inplace=True)
-
+  
 def clean_text(text):
     text = text.lower()
     text = text.replace('\n', ' ')
     text = re.sub(r'\d+', '', text)
     text = text.translate(str.maketrans('', '', string.punctuation))
     return text
-
+  
 def remove_stopwords(text):
     return ' '.join([word for word in text.split() if word not in stopwords.words('portuguese')])
-
-df_all['ementa_clean'] = df_all['ementa'].apply(clean_text)
-df_all['ementa_clean'] = df_all['ementa_clean'].apply(remove_stopwords)
-
-categoria = modelo.predict(df_all['ementa_clean'])
-
-df_all['classificacao'] = categoria
-
-obj = alt.Chart(df_all).mark_bar().encode(
-    x='ano',
-    y='count()',
-    color='classificacao'
-)
-
+  
 col1, col2, col3 = st.columns(3)
 
 with col1:
